a158ed46485f2557f01ab448995c93f43799d323
gomme <gomme@DESKTOP-NB15H72>
2019-02-25 18:40:09 -0700

    Added a output message for push

--- Assignment2/src/testFile.java
+++ Assignment2/src/testFile.java
@@ -3,6 +3,8 @@ public class testFile {
 
 	public static void main(String[] args) {
 		System.out.println("This is a test to see if it is working");
+		
+		System.out.println("This is an update so i can push!");
 
 	}
 
